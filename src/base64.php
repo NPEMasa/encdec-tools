13409d1f1cc15ed8c93f135e9b85b33c9db1b1be
NPEMasa <npemasa@outlook.jp>
2020-07-14 19:57:05 +0900

    change src js and css

--- src/base64.php
+++ src/base64.php
@@ -24,8 +24,7 @@ if(isset($_POST['text']) && isset($_POST['flg'])){
     <title> encode && decode tool </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/base64.css">
 
   </head>
@@ -62,7 +61,7 @@ if(isset($_POST['text']) && isset($_POST['flg'])){
     </main>
 
 
-    <script src="js/jquery-3.5.1.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.js"></script>
   </body>
 </html>
